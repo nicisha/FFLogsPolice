0a09649e7ea5e9ff7a9fc24602e76972734ebf14
Hasee <Hasee@DESKTOP-RKSA4V9>
2022-05-23 20:15:59 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FFLogsPolice
+{
+    public partial class FFLogsPolice : Form
+    {
+        public FFLogsPolice()
+        {
+            InitializeComponent();
+        }
+    }
+}
